0471283e6fcbbdb21fa9bde73086ff6e9d64f159
Mahmoud ElNaggar <naggar218@gmail.com>
2022-12-21 19:32:42 +0100

    remove unresolved module

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { Loading } from "./loading";
-export { UnitToggle } from "./unit-toggle";
 export { WeatherCard } from "./weather-card";
 export { WeatherForecast } from "./weather-forecast";
 export { Header } from "./header";
